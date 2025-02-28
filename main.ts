0d594d9531b180a40aa77217038ee63038e867c9
Geometric7 <thegeometric7@gmail.com>
2025-02-28 13:49:44 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
-let isOpen = 0
-let xdeg = 90
-servos.P0.setAngle(180)
-servos.P1.setAngle(0)
+let isOpen = 0;
+let xdeg = 90;
+servos.P0.setAngle(180);
+servos.P1.setAngle(0);
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
         if (isOpen == 1) {
             servos.P0.setAngle(180 + xdeg);
-            servos.P1.setAngle(0 - xdeg);
+            servos.P1.setAngle(-xdeg);
             isOpen += -1;
+            basic.pause(500);
             return;
         } else if (isOpen == 0) {
             servos.P0.setAngle(180 - xdeg);
             servos.P1.setAngle(xdeg);
             isOpen += 1;
+            basic.pause(500);
             return;
         } else return;
     } else return;
