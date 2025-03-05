b27023a44ca89fe87875c74b2fb130f9b5c0f08a
Geometric7 <thegeometric7@gmail.com>
2025-03-05 14:03:19 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-let isOpen = false, rotSpeed = 100, rotTime = 500;
+let isOpen = false, rotSpeed = 5, rotTime = 500;
 servos.P0.setRange(70, 110);
 servos.P1.setRange(70, 110);
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
-        servos.P0.run(isOpen ? rotSpeed : -rotSpeed);
-        servos.P1.run(isOpen ? -rotSpeed : rotSpeed);
+        servos.P0.run(isOpen ? -rotSpeed * 2 : rotSpeed);
+        servos.P1.run(isOpen ? rotSpeed : -rotSpeed * 2);
+        isOpen = !isOpen;
         basic.pause(rotTime);
         servos.P0.stop();
         servos.P1.stop();
