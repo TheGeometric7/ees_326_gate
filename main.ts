fe47893abbdf3ba60352626a9038b26a3307fb08
Geometric7 <thegeometric7@gmail.com>
2025-03-06 14:22:08 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
-let isOpen = false, rotSpeed = 5, rotTime = 500;
-servos.P0.setRange(70, 110);
-servos.P1.setRange(70, 110);
+let isGateOpen = false
+let rotSpeed = 5
+let negRotSpeed = -5
+let rotTime = 700
+servos.P0.setRange(70, 110)
+servos.P1.setRange(70, 110)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
-        servos.P0.run(isOpen ? -rotSpeed * 2 : rotSpeed);
-        servos.P1.run(isOpen ? rotSpeed : -rotSpeed * 2);
-        isOpen = !isOpen;
-        basic.pause(rotTime);
-        servos.P0.stop();
-        servos.P1.stop();
-    };
-});
+        if (isGateOpen == false) {
+            servos.P0.run(rotSpeed)
+            servos.P1.run(negRotSpeed)
+        } else {
+            servos.P0.run(negRotSpeed)
+            servos.P1.run(rotSpeed)
+        }
+        isGateOpen = !(isGateOpen)
+        basic.pause(rotTime)
+        servos.P0.stop()
+        servos.P1.stop()
+    }
+})
